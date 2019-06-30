736dcf7c934f5bf5a245daf1f326eb6a127c940a
o22y <simplewaay@gmail.com>
2019-06-30 21:39:13 +0300

    Сделана валидность полей

--- js/main.js
+++ js/main.js
@@ -110,3 +110,37 @@ mainPin.addEventListener('mouseup', function () {
   address.value = '';
   address.value += ((pinCoordinate.left + PIN_WIDTH / 2) + ', ' + (pinCoordinate.top + PIN_HEIGHT / 2));
 });
+
+// Изменение минимальной цены при разных типах жилья
+var houseType = document.querySelector('#type');
+var housePrice = document.querySelector('#price');
+var changeValue = function () {
+  if (houseType.value === 'bungalo') {
+    housePrice.min = '0';
+    housePrice.placeholder = '0';
+  } if (houseType.value === 'flat') {
+    housePrice.min = '1000';
+    housePrice.placeholder = '1000';
+  } if (houseType.value === 'house') {
+    housePrice.min = '5000';
+    housePrice.placeholder = '5000';
+  } if (houseType.value === 'palace') {
+    housePrice.min = '10000';
+    housePrice.placeholder = '10000';
+  }
+};
+
+houseType.addEventListener('change', changeValue);
+
+// Синхронизация полей въезда и выезда
+var timeIn = document.querySelector('#timein');
+var timeOut = document.querySelector('#timeout');
+var changeTimeIn = function () {
+  timeOut.value = timeIn.value;
+};
+var changeTimeOut = function () {
+  timeIn.value = timeOut.value;
+};
+
+timeIn.addEventListener('change', changeTimeIn);
+timeOut.addEventListener('change', changeTimeOut);
