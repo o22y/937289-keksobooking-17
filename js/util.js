fad1bea29b7de4a06f74e0f50b694a1012025d75
o22y <simplewaay@gmail.com>
2019-07-25 19:25:57 +0300

    Внесены правки

--- js/util.js
+++ js/util.js
@@ -1,9 +1,7 @@
 'use strict';
-
-window.mapPins = document.querySelector('.map__pins'); // window.util.mapPins
-window.mainPin = document.querySelector('.map__pin--main'); // window.util.mainPin
-
 (function () {
+  window.mapPins = document.querySelector('.map__pins');
+  window.mainPin = document.querySelector('.map__pin--main');
   // Функция рандомных чисел
   var randomInteger = function (min, max) {
     var rand = min - 0.5 + Math.random() * (max - min + 1);
