bbd94a7140d35e1d2229f66fd1a057518684b597
o22y <simplewaay@gmail.com>
2019-07-29 21:34:44 +0300

    Внесены правки

--- js/active.js
+++ js/active.js
@@ -1,34 +1,45 @@
 'use strict';
 
 (function () {
+  var map = document.querySelector('.map');
+  var form = document.querySelector('.ad-form');
   // Активация страницы
   var activatePage = function () {
-    var map = document.querySelector('.map');
     // Активация карты
     map.classList.remove('map--faded');
     // Активация формы
-    var form = document.querySelector('.ad-form');
     form.classList.remove('ad-form--disabled');
-    renderPins(window.data.data);
     window.form.activeFieldsets();
     window.mainPin.removeEventListener('click', activatePage);
   };
   // Отрисовка меток
   var renderPins = function (data) {
     var pin = document.querySelector('#pin').content.querySelector('.map__pin');
-    for (var i = 0; i < window.data.length; i++) {
+    for (var i = 0; i < data.length; i++) {
       var element = pin.cloneNode(true);
-      element.style.left = window.data[i].location.x + 'px';
-      element.style.top = window.data[i].location.y + 'px';
-      element.querySelector('img').src = window.data[i].author.avatar;
-      element.querySelector('img').alt = window.data[i].offer.type;
+      element.style.left = data[i].location.x + 'px';
+      element.style.top = data[i].location.y + 'px';
+      element.querySelector('img').src = data[i].author.avatar;
+      element.querySelector('img').alt = data[i].offer.type;
 
       window.mapPins.appendChild(element);
     }
   };
+
+  var activeScreen = function (data) {
+    renderPins(data);
+    window.succesData = data;
+    activatePage();
+  };
+
+  var dataToLocalData = function () {
+    window.data.load(activeScreen);
+  };
+
   window.active = {
     renderPins: renderPins,
     activatePage: activatePage,
+    dataToLocalData: dataToLocalData
   };
 
 })();
--- js/data.js
+++ js/data.js
@@ -1,34 +1,37 @@
 'use strict';
 
 (function () {
-  var xhr = new XMLHttpRequest();
-  var onError = function (message) {
-    console.error(message);
+
+var URL = 'https://js.dump.academy/keksobooking/data';
+
+var load = function (onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function (evt) {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.timeout = 10000; // 10s
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.open('GET', URL);
+    xhr.send();
   };
 
-  var onSuccess = function (data) {
-    console.log(data);
+  window.data = {
+    load: load
   };
 
-  xhr.addEventListener('load', function (evt) {
-    if (xhr.status === 200) {
-      onSuccess(xhr.response);
-      window.data = JSON.parse(xhr.responseText);
-    } else {
-      onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
-    }
-  });
-
-  xhr.addEventListener('error', function () {
-    onError('Произошла ошибка соединения');
-  });
-
-  xhr.addEventListener('timeout', function () {
-    onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-  });
-
-  xhr.open('GET', 'https://js.dump.academy/keksobooking/data');
-  xhr.send();
-
-  xhr.timeout = 10000; // 10s
 })();
